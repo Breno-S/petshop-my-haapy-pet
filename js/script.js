4f4ef2b6cbc57f1e311d58d2f86bc159751541b6
Nabo <110341514+Navomeister@users.noreply.github.com>
2023-04-20 14:47:00 -0300

    Atualização no script
    validação de data e cartão

--- js/script.js
+++ js/script.js
@@ -17,11 +17,29 @@ const cardViewNumber = document.querySelector('#card-user-number');
 const cardViewCvv = document.querySelector('#card-user-cvv');
 const cardViewDate = document.querySelector('#card-user-date');
 
+var numCartao;
+
+var dataValida = false;
+var ta_serto = false;
+var ta_serto_os2 = false;
+var cartoes_aceitos = {
+  visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+  mastercard:
+    /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/,
+};
 
 inputNumber.onblur = (e) => {
 	const value = e.target.value;
 	const valueReplace = value.replaceAll(' ', '')
+	numCartao = value;
 
+	if (validateCard(value)){
+		ta_serto = true;
+		} else {
+		  ta_serto = false;
+		  btnSubmit.classList.add('disable')
+		  return false;
+	  }
 
 	if(value.length <= 0){
 		const message = "Preenchimento obrigatório!"
@@ -37,12 +55,14 @@ inputNumber.onblur = (e) => {
 		inputNumberInfo.classList.add('visible')
 		btnSubmit.classList.add('disable')
 		return false;
+	
 	}
 
 	inputNumberInfo.querySelector('.message').innerText = ''
 	inputNumberInfo.classList.remove('visible')
 
 	canSubmit();
+
 }
 
 inputName.onblur = (e) => {
@@ -93,7 +113,14 @@ inputValidate.onblur = (e) => {
 	}
 
 	validaData(value);
-	canSubmit();
+
+	if (dataValida) {
+		canSubmit();
+	}
+	else{
+		return false;
+	}
+	
 }
 
 btnRotateCard.addEventListener('click', (e) => {
@@ -115,6 +142,7 @@ const handleName = (e) => {
 const handleNumber = (e) => {
 
 	setTimeout(() => {
+		let apagou = false;
 		let teste = '';
 
 		const value = e.target.value
@@ -151,7 +179,7 @@ const handleCvv = (e) => {
 		const value = e.target.value
 
 
-		cardViewCvv.value = value
+		cardViewCvv.innerText = "•••"
 
 	}, 0)
 	
@@ -185,17 +213,19 @@ function validaData(data) {
 		var yyyy = hoje.getFullYear();
 		let dataValidade = data.split('/');
 		
-		if(Number(dataValidade[1]) <= Number(yyyy) && (Number(dataValidade[0]) >= 1 || Number(dataValidade[0]) <= 12)){
+		if(Number(dataValidade[1]) <= Number(yyyy) && (Number(dataValidade[0]) >= 1 && Number(dataValidade[0]) <= 12)){
 			if (Number(dataValidade[1]) < Number(yyyy) || Number(dataValidade[0]) < Number(mm)) {
 				const message = `Erro: Cartão Vencido`
 				inputValidateInfo.querySelector('.message').innerText = message
 				inputValidateInfo.classList.add('visible')
 				btnSubmit.classList.add('disable')
+				dataValida = false;
 			}
 			else{
 				inputValidateInfo.querySelector('.message').innerText = ""
 				inputValidateInfo.classList.remove('visible');
 				btnSubmit.classList.remove('disable');
+				dataValida = true;
 			}	
 		}
 		else if (Number(dataValidade[0]) < 1 || Number(dataValidade[0]) > 12) {
@@ -203,11 +233,13 @@ function validaData(data) {
 			inputValidateInfo.querySelector('.message').innerText = message
 			inputValidateInfo.classList.add('visible')
 			btnSubmit.classList.add('disable')
+			dataValida = false;
 		}
 		else{
 			inputValidateInfo.querySelector('.message').innerText = ""
 			inputValidateInfo.classList.remove('visible');
 			btnSubmit.classList.remove('disable');
+			dataValida = true;
 		}	
 	}
 }
@@ -216,8 +248,23 @@ inputCvv.onfocus = () => {
 	cardBox.classList.remove('rotate')
 }
 
-inputCvv.onblur = () => {
-	cardBox.classList.add('rotate')
+inputCvv.onblur = (e) => {
+	cardBox.classList.add('rotate');
+	const value = e.target.value;
+	if (validateCVV(numCartao, value)){
+		if (ta_serto) {
+			ta_serto_os2 = true;
+		}
+	} 
+	else {
+		ta_serto_os2 = false;
+		btnSubmit.classList.add('disable');
+		return false;
+	}
+
+	if (ta_serto && ta_serto_os2) {
+		btnSubmit.classList.remove('disable')
+	}
 	canSubmit();
 }
 
@@ -225,15 +272,66 @@ function canSubmit(){
 	
 	const inputs = document.querySelectorAll('input')
 
-	for(let i = 0; i < inputs.length; i++){
-
+	for(let i = 1; i < 5; i++){
 		if(inputs[i].value.length <= 0){
 			btnSubmit.classList.add('disable')
 			return false;
 		}
 	}
-
-	btnSubmit.classList.remove('disable')
+	if (ta_serto && ta_serto_os2 && dataValida) {
+		btnSubmit.classList.remove('disable')
+	}
 }
 
 canSubmit()
+
+function validateCard(cartao) {
+  // remove all non digit characters
+  let texto = String(cartao)
+  var value = texto.replace(/\D/g, "");
+  var sum = 0;
+  var shouldDouble = false;
+  // loop through values starting at the rightmost side
+  for (var i = value.length - 1; i >= 0; i--) {
+    var digit = parseInt(value.charAt(i));
+
+    if (shouldDouble) {
+      if ((digit *= 2) > 9) digit -= 9;
+    }
+
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  var valid = sum % 10 == 0;
+  var accepted = false;
+
+  // loop through the keys (visa, mastercard, amex, etc.)
+  Object.keys(cartoes_aceitos).forEach(function (key) {
+    var regex = cartoes_aceitos[key];
+    if (regex.test(value)) {
+      accepted = true;
+    }
+  });
+
+  return valid && accepted;
+}
+
+function validateCVV(creditCard, cvv) {
+  // remove all non digit characters
+  let textoCartao = String(creditCard);
+  let textoCVV = String(cvv);
+  var cartao = textoCartao.replace(/\D/g, "");
+  var numeros = textoCVV.replace(/\D/g, "");
+    // validar se o cvv tem 3 digitos
+  if (/^\d{3}$/.test(numeros)) {
+    return true;
+  }
+
+  return false;
+}
+
+
+
+
+

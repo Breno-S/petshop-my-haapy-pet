87401d7415ac3aca6db8edf83f1c08c6f3fd4cd5
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-05-25 17:20:44 -0300

    Update proc_agendamento.php
    calcular preço do agendamento; falta utilizar esta variável no campo de valor_total da tabela de agendamento

--- php/proc_agendamento.php
+++ php/proc_agendamento.php
@@ -3,11 +3,37 @@
     include_once('conexao.php');
     session_start();
 
+    $taxa_transporte = 0;
+    $preco_servico = 0;
+    $valor_total = 0;
+
     $id_cliente = intval($_SESSION['idCliente']);
     $id_horario = intval(filter_input(INPUT_POST, 'horario', FILTER_SANITIZE_NUMBER_INT));
+    $servico = filter_input(INPUT_POST, 'servico');
+
+    // definir o preco do serviço
+    switch ($servico) {
+        case 'Banho':
+        case 'Tosa':
+            $preco_servico = 100;
+            break;
+        case 'Hotel':
+            $preco_servico = 200;
+            break;
+        case 'Consulta':
+            $preco_servico = 400;
+            break;
+        case 'Cirurgia':
+            $preco_servico = 600;
+            break;
+        case 'Especialidade':
+            $preco_servico = 500;
+            break;
+    }
 
     if (isset($_SESSION["transporte"])) {
         $transporte = true;
+        $taxa_transporte = 80;
     } else {
         $transporte = false;
     }
@@ -25,6 +51,8 @@
         for ($i=0; $i < count($array_id_pets); $i++) { 
             $id_animal = $array_id_pets[$i];
             mysqli_query($conn, "INSERT INTO rel_agendamento (fk_agendamento, fk_animal) VALUES ($id_agendamento, $id_animal)");
+            
+            $valor_total += $preco_servico + $taxa_transporte;
         }
 
         $horario_reservado = mysqli_query($conn, "UPDATE horarios_disponiveis SET reservado = 1 WHERE idHorario = $id_horario");

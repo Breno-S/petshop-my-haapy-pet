dc14e15ea3d27297f639260f31215bce193b12dd
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-05-29 14:30:11 -0300

    Update proc_agendamento.php
    salva preço total no banco; mais comentários no código

--- php/proc_agendamento.php
+++ php/proc_agendamento.php
@@ -11,7 +11,7 @@
     $id_horario = intval(filter_input(INPUT_POST, 'horario', FILTER_SANITIZE_NUMBER_INT));
     $servico = filter_input(INPUT_POST, 'servico');
 
-    // definir o preco do serviço
+    // Definir o preco do serviço
     switch ($servico) {
         case 'Banho':
         case 'Tosa':
@@ -38,16 +38,17 @@
         $transporte = false;
     }
     
-    // obter os animais selecionados
+    // Obter os animais selecionados
     $pets = array_slice($_POST, 7);   
     $array_id_pets = array_values($pets);
 
-    // criar registro de agendamento
+    // Criar registro de agendamento
     $reserva_horario = mysqli_query($conn, "INSERT INTO agendamento (id_cliente, id_horario) VALUES ($id_cliente, $id_horario);");
 
     if (mysqli_insert_id($conn)) {
         $id_agendamento = mysqli_insert_id($conn);
 
+        // Calcular valor total
         for ($i=0; $i < count($array_id_pets); $i++) { 
             $id_animal = $array_id_pets[$i];
             mysqli_query($conn, "INSERT INTO rel_agendamento (fk_agendamento, fk_animal) VALUES ($id_agendamento, $id_animal)");
@@ -55,8 +56,11 @@
             $valor_total += $preco_servico + $taxa_transporte;
         }
 
+        // Muda o horário para reservado
         $horario_reservado = mysqli_query($conn, "UPDATE horarios_disponiveis SET reservado = 1 WHERE idHorario = $id_horario");
 
+        // Salva o valor total do agendamento
+        mysqli_query($conn, "UPDATE agendamento SET valor_total = $valor_total WHERE idAgendamento = $id_agendamento");
     }
 
     header('Location:../agendamento.php');
